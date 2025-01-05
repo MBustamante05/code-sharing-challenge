8cac771ec600d041232e88ae50d3b2030160b3cf
MBustamante05 <securekali85@gmail.com>
2025-01-04 19:12:00 -0500

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import { PORT } from '../config.js';
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+import { PORT, NODE_ENV } from "../config.js";
+const __dirname = path.resolve();
 
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: "http://localhost:5173",
   credentials: true,
+};
+
+import { connectDB } from "./lib/db.js";
+import codeRouter from "./routes/code.route.js";
+
+if (NODE_ENV !== "production") {
+  app.use(cors(corsOptions));
 }
-
-import { connectDB } from './lib/db.js';
-import codeRouter from './routes/code.route.js';
-
-app.use(cors(corsOptions));
+if(NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 app.use(express.json());
 
-app.use('/api/v1/code', codeRouter);
+app.use("/api/v1/code", codeRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
-})
+});
--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/code',
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1/code" : "/api/v1/code",
   withCredentials: true
 });
 
